3bd724b15e783c4c4d57431d2540f87b4d76b0dd
Romina Trazzi <romina.trazzi@posta.it>
2024-02-15 20:12:08 +0100

    Refactoring cargo train logic

--- src/script.js
+++ src/script.js
@@ -8,19 +8,16 @@ const trainLetter = {
 
 let userString;
 
-// 2. Create Regex expression for valid patterns
+// 2. Create Regex expressions for valid patterns
 const regex = /^(?=.*[HRPC])[HRPC]{3,5}$/i
 
 // 3. Check if the user string has a valid pattern
 const checkPattern = (userInput) => {
-    
-    // 1. Grab the whole user input value
-    userString = document.getElementById("train_string").value;
-    
-    // 2. Regex test
+        
+    // 3.1. Regex tests
     let regexResult = regex.test(userInput);
 
-    // 3. Regex test boolean results
+    // 3.2. Regex test boolean results
     if (regexResult) {
         ASCII_Train_Generator(userString);
         document.getElementById("train_string").value = ""; // Clear input field
@@ -29,47 +26,54 @@ const checkPattern = (userInput) => {
     }
 }
 
-// 3. Fire the checkPattern function
-document.getElementById("print_ASCII").addEventListener("click", () => checkPattern(userString)); 
-
+// Fire the checkPattern function
+document.getElementById("print_ASCII").addEventListener("click", () => {
+    userString = document.getElementById("train_string").value; // Grab the updated value of the user input string
+    checkPattern(userString);
+});
 
 // 4. Function that generates and visualizes the Ascii Train code
 function ASCII_Train_Generator (userInput) {
 
-    // 1. Capitalize and explode the user string in single elements of an array
+    // 4.1. Capitalize and explode the user string in single elements of an array
     let stringSplit = userInput.toUpperCase().split("");
 
-    // 2. Compare each letter to the train variables and get train values
+    // 4.2. Compare each letter to the train variables and get train values
     let carriage = [];
-    
-    stringSplit.forEach((letter, index) => {
-        if (index === 0 && letter === "H") {
-            carriage.push(trainLetter.H[0]);
-        } else if (index === stringSplit.length - 1 && letter === "H") {
-            carriage.push(trainLetter.H[1]);
-        } else if (letter in trainLetter) {
-            carriage.push(trainLetter[letter]);
-        }
-    });
-    
-    // 3. Add the separator
+
+    // 4.3. Check if train is a Cargo or a People one
+    const cargoCondition = stringSplit.some((letter  => letter === "C"));
+
+    if (cargoCondition) {
+        // Algorithm for Cargo train
+        carriage.push(trainLetter.H[0]);
+        // Conta le C.
+        // Se è pieno errore
+        // Se è con ristorante o passeggeri, errore
+    } else {
+        // Algorithm for People train
+        stringSplit.forEach((letter, index) => {
+            if (index === 0 && letter === "H") {
+                carriage.push(trainLetter.H[0]);
+            } else if (index === stringSplit.length - 1 && letter === "H") {
+                carriage.push(trainLetter.H[1]);
+            } else if (letter in trainLetter) {
+                carriage.push(trainLetter[letter]);
+            }
+        });
+
+    }
+
+    // 4.4. Add the separator
     const carriageJoined = carriage.join("::");
 
- 
-    // 4. Write the solution into an HTML paragraph
-    document.getElementById("result").innerHTML = carriageJoined;
-
+    return carriageJoined
 }
-    
+
+
+// 4.5. Write the solution into an HTML paragraph
+document.getElementById("result").innerHTML = carriageJoined;    
 
 
    
    
-  
-
-
-
-
-
- // 1. Store the number of the letters
-    // let carriageQuantity = userInput.length;
