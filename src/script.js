001e17b42ff47d18a5f76668ab1625e736623f6f
Romina Trazzi <romina.trazzi@posta.it>
2024-02-16 04:45:35 +0100

    Cargo train pt 2 + initial refactoring

--- src/script.js
+++ src/script.js
@@ -1,5 +1,8 @@
-// 1. TrainLetter object
-const trainLetter = {
+/*=============================================
+    =           1. SETTING DATASET            =
+=============================================*/
+
+const trainLetterObj = {
     H: ["<HHHH", "HHHH>"],
     R: "|hThT|",
     P: "|OOOO|",
@@ -8,98 +11,132 @@ const trainLetter = {
 
 let userString;
 
-// 2. Create Regex expressions for valid patterns
+let carriage = [];
+
+/*=====  End of Section Setting Dataset ======*/
+
+
+/*=====================================================================
+=            2. CHECK USER STRING PATTERN (ES 6 function)           =
+=======================================================================*/
+
+//  Create Regex expressions for valid patterns
 const regex = /^(?=.*[HRPC])[HRPC]{3,5}$/i
 
-// 3. Check if the user string has a valid pattern
 const checkPattern = (userInput) => {
         
-    // 3.1. Regex tests
     let regexResult = regex.test(userInput);
 
-    // 3.2. Regex test boolean results
     if (regexResult) {
         
         // Timing function & loading message
-        document.getElementById("result").innerHTML = "Generating ASCII train..."; 
+        document.getElementById("result").textContent = "Generating ASCII train..."; 
         
-        // Coding function
+        // ASCII GENERATOR FUNCTION
         ASCII_Train_Generator(userString);
 
         // Clear input field 
-        document.getElementById("train_string").value = "";   
+        resetInputField();
     
     } else {
         alert("Please try again. Your string pattern is wrong");
-        document.getElementById("train_string").value = ""; 
+        resetInputField();
     }
 }
 
-// Fire the checkPattern function
-document.getElementById("print_ASCII").addEventListener("click", () => {
-    userString = document.getElementById("train_string").value; // Grab the updated value of the user input string
-    checkPattern(userString);
-});
+/*====  End of Section Check user string pattern  ====*/
 
-// 4. Function that generates and visualizes the Ascii Train code
+
+
+/*==============================================================
+=       3. GENERATE ASCII TRAIN CODE (ES 5 function)           =
+=================================================================*/
+
 function ASCII_Train_Generator (userInput) {
-
-    console.log('Start the creation of Ascii train');
-    
-    // 4.1 Empty Array data tracker
-    let carriage = [];
-    
-    // 4.2. Capitalize and explode the user string in single elements of an array
+ 
     let stringSplit = userInput.toUpperCase().split("");
 
-    // 4.3. Check if train is a Cargo or a People one
-    const cargoCondition = stringSplit.some((letter  => letter === "C"));
+    // Check if the train is a Cargo or a People one
+    let containsCargo = stringSplit.some((letter  => letter === "C"));
     
-    if (cargoCondition) {
-        // 4.3.1. Algorithm for Cargo train
-        carriage.push(trainLetter.H[0]);
-
+    if (containsCargo) {
+        
+        // 3.1. Algorithm for Cargo train
         let cargoContainer = stringSplit.filter((letter => letter === "C"));
+        let numberOfCargo = cargoContainer.length;
 
         // Check for string errors
-        if (cargoContainer.length > 4) {
+        if (numberOfCargo > 4) {
             alert("Error: cannot fill a full train");
-            document.getElementById("train_string").value = ""; 
-        } else if (stringSplit.includes('R') || stringSplit.includes('C')) {
+            resetInputField();
+            return
+        } else if (!stringSplit.includes('H')) {
+            alert("Error: cargo train must have a locomotive");
+            resetInputField();
+            return
+        } else if (stringSplit.includes('R') || stringSplit.includes('P')) {
             alert("Error: this is a Cargo train. R and P are for People trains");
-            document.getElementById("train_string").value = ""; 
-        // Create the cargo train
+            resetInputField();
+            return
+        // Create Cargo train
         } else {
-            const lengthUserInput = stringSplit.length;
-            let totalCargoCarriage = lengthUserInput - cargoContainer.length;
-            console.log(totalCargoCarriage);
-
+            carriage.push(trainLetterObj.H[0]);
+            carriage.push(...Array(numberOfCargo).fill(trainLetterObj.C[0]));
         }
 
     } else {
-        // 4.3.2 Algorithm for People train
+        // 3.2 Algorithm for People train
         stringSplit.forEach((letter, index) => {
             if (index === 0 && letter === "H") {
-                carriage.push(trainLetter.H[0]);
+                carriage.push(trainLetterObj.H[0]);
             } else if (index === stringSplit.length - 1 && letter === "H") {
-                carriage.push(trainLetter.H[1]);
-            } else if (letter in trainLetter) {
-                carriage.push(trainLetter[letter]);
+                carriage.push(trainLetterObj.H[1]);
+            } else if (letter in trainLetterObj) {
+                carriage.push(trainLetterObj[letter]);
             }
         });
     }
     
-    // 4.4. Add the separator
+    // Add the separator
     const carriageJoined = carriage.join("::");
 
-    console.log("carriageJoined:", carriageJoined); 
+
+
+
+/*==  End of Section Generate Ascii Train Code ===*/
+
+
+
+
+
+
+
+
+// 4. Generate and visualize the Ascii Train code 
+
 
     // 4.5. Write the solution into an HTML paragraph
     document.getElementById("result").textContent = carriageJoined; 
-    
-    return carriageJoined
+
 }
 
+/*=============================================
+    =    4.       FIRING FUNCTIONS            =
+==============================================*/
+
+// Fire the checkPattern function
+document.getElementById("print_ASCII").addEventListener("click", () => {
+    // Grab the updated value of the user input string
+    userString = document.getElementById("train_string").value; 
+    checkPattern(userString);
+});
+
+// Clear input field (ES 6 arrow function)
+const resetInputField = () => {
+    document.getElementById("train_string").value = "";
+}
+
+/*=====  End of Section firing function  ======*/
 
 
    
